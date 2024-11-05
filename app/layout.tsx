61522812020a58aabfac2242aabb4cc70fd15007
racine14creator <bisimwagrace596@gmail.com>
2024-11-05 04:33:31 +0000

    Add favicon of my github profile

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        <link rel="shortcut icon" href="https://avatars.githubusercontent.com/u/148460524?v=4" type="image/x-icon" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
